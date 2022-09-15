59e398fd9cff4aeeda042667568cbd9be5473b4c
Sheikh -akib <113592311+Sheikh-akib@users.noreply.github.com>
2022-09-15 20:22:47 +0600

    Add files via upload

---
+++ A program that takes a number input and determines whether it is positive, negative or zero.py
@@ -0,0 +1,10 @@
+#A program that takes a number input and determines whether it is positive, negative or zero
+
+num= float(input("Enter a number"))
+if num>=0:
+	 if num == 0:
+	 	print ("it is zero")
+	 else:
+	 	print("it is a posative number")
+else:
+	print("it is a negative number")
